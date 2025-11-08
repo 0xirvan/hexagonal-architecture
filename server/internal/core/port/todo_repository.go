734cfb1ff41f160d76ccc61e30317382a169dc51
irvan pramana <darknet1255@gmail.com>
2025-11-09 05:13:11 +0700

    feat(port): extend TodoRepository interface with additional methods for todo operations

--- server/internal/core/port/todo_repository.go
+++ server/internal/core/port/todo_repository.go
@@ -10,4 +10,9 @@ type TodoRepository interface {
 	Create(ctx context.Context, todo *domain.Todo) (*domain.Todo, error)            // returns created todo
 	FindByID(ctx context.Context, id uint) (*domain.Todo, error)                    // returns todo or nil if not found
 	FindPaginated(ctx context.Context, page, size int) ([]*domain.Todo, int, error) // returns slice of todos and total count
+	FindAll(ctx context.Context) ([]*domain.Todo, error)                            // returns all todos
+	MarkAsDone(ctx context.Context, id uint) error                                  // marks a todo as done, returns error if not found
+	MarkAsUndone(ctx context.Context, id uint) error                                // marks a todo as undone, returns error if not found
+	Delete(ctx context.Context, id uint) error                                      // deletes a todo, returns error if not found
+	Update(ctx context.Context, todo *domain.Todo) (*domain.Todo, error)            // updates a todo, returns updated todo or error if not found
 }
