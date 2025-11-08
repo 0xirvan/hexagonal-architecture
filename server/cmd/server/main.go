fd05107b7df665fbbd7e26d72d78a2a220de566e
irvan pramana <darknet1255@gmail.com>
2025-11-09 01:11:30 +0700

    feat(http): implement HTTP router with middleware setup

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log/slog"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/0xirvan/tta-svelte-go/server/internal/adapter/config"
+	"github.com/0xirvan/tta-svelte-go/server/internal/adapter/delivery/http"
 )
 
 func main() {
@@ -22,5 +28,40 @@ func run() error {
 	}
 
 	slog.Info("config loaded successfully", slog.Any("config", cfg))
+
+	// Init router
+	router := http.NewRouter(cfg.HTTP)
+
+	// Start server in antoher goroutine
+	listenAddr := fmt.Sprintf("%s:%s", cfg.HTTP.Host, cfg.HTTP.Port)
+	slog.Info("starting HTTP server on", slog.String("address", listenAddr))
+
+	serverErrCh := make(chan error, 1)
+	go func() {
+		serverErrCh <- router.Start(listenAddr)
+	}()
+
+	// Graceful shutdown
+	// Wait for server error or termination signal
+	select {
+	case <-serverErrCh:
+		return fmt.Errorf("HTTP server stopped unexpectedly: %w", <-serverErrCh)
+	case <-waitForSignal():
+		slog.Info("termination signal received, shutting down server...")
+	}
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := router.Shutdown(shutdownCtx); err != nil {
+		return err
+	}
 	return nil
+}
+
+// waitForSignal waits for OS interrupt or termination signals
+func waitForSignal() <-chan os.Signal {
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+	return sigCh
 }
---
+++ server/internal/adapter/delivery/http/router.go
@@ -0,0 +1,35 @@
+package http
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/0xirvan/tta-svelte-go/server/internal/adapter/config"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+// Router wraps the echo.Echo instance
+type Router struct {
+	*echo.Echo
+}
+
+// NewRouter creates a new Echo router instance
+func NewRouter(
+	cfg *config.HTTP,
+) *Router {
+	e := echo.New()
+
+	setupMiddleware(e, cfg)
+	return &Router{e}
+}
+
+// setupMiddleware configures the middleware for the Echo instance
+func setupMiddleware(e *echo.Echo, cfg *config.HTTP) {
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: strings.Split(cfg.AllowedOrigins, ","),
+		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodPatch, http.MethodDelete, http.MethodOptions},
+	}))
+}
