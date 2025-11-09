1b38943a553432b07ec3e321e8c09ffc5ad8a2d5
irvan pramana <darknet1255@gmail.com>
2025-11-10 06:56:24 +0700

    feat(domain): add global domain errors for invalid ID and request body

--- server/internal/core/domain/errors.go
+++ server/internal/core/domain/errors.go
@@ -2,6 +2,13 @@ package domain
 
 import "errors"
 
+// global domain errors
+var (
+	ErrInvalidID          = errors.New("invalid id format")
+	ErrInvalidRequestBody = errors.New("invalid request body")
+)
+
+// Todo domain errors
 var (
 	ErrTodoNotFound = errors.New("todo not found")
 )
