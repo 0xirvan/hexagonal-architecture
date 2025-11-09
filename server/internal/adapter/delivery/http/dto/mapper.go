b7d79fb3af0e1235450d785a2d2a2678828069ab
irvan pramana <darknet1255@gmail.com>
2025-11-10 06:59:10 +0700

    feat(dto): implement MapDomainError function to map domain errors to HTTP status codes

---
+++ server/internal/adapter/delivery/http/dto/mapper.go
@@ -0,0 +1,22 @@
+package dto
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/0xirvan/tta-svelte-go/server/internal/core/domain"
+)
+
+// MapDomainError maps domain errors to HTTP status codes and messages(pure error string)
+func MapDomainError(err error) (int, string) {
+	switch {
+	case errors.Is(err, domain.ErrTodoNotFound):
+		return http.StatusNotFound, err.Error()
+	case errors.Is(err, domain.ErrInvalidID):
+		return http.StatusBadRequest, err.Error()
+	case errors.Is(err, domain.ErrInvalidRequestBody):
+		return http.StatusBadRequest, err.Error()
+	}
+
+	return http.StatusInternalServerError, err.Error()
+}
