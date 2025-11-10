834a6cd32f79bf90fdb08cbc742df9ccd5a81e8a
irvan pramana <darknet1255@gmail.com>
2025-11-10 09:26:34 +0700

    refactor(dto): add validation tags to CreateTodoRequest and UpdateTodoRequest

--- server/internal/adapter/delivery/http/dto/todo/dto.go
+++ server/internal/adapter/delivery/http/dto/todo/dto.go
@@ -2,17 +2,12 @@ package todo
 
 // CreateTodoRequest represents the payload for creating a new todo item
 type CreateTodoRequest struct {
-	Title       string `json:"title"`
-	Description string `json:"description"`
+	Title       string `json:"title" validate:"required,min=3"`
+	Description string `json:"description,omitempty" validate:"omitempty,max=255"`
 }
 
 // UpdateTodoRequest represents the payload for updating an existing todo item
 type UpdateTodoRequest struct {
-	Title       *string `json:"title,omitempty"`
-	Description *string `json:"description,omitempty"`
-}
-
-// MarkTodoRequest represents the payload for marking a todo item as done or undone
-type MarkTodoRequest struct {
-	IsDone bool `json:"is_done"`
+	Title       *string `json:"title" validate:"omitempty,min=3"`
+	Description *string `json:"description,omitempty" validate:"omitempty,max=255"`
 }
