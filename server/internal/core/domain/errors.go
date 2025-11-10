a0c67e7e5f077c366a55523bc211d82a1c1382b2
irvan pramana <darknet1255@gmail.com>
2025-11-10 09:26:13 +0700

    refactor(errors): move ValidationErrors type and Error method to domain errors file

--- server/internal/core/domain/errors.go
+++ server/internal/core/domain/errors.go
@@ -8,6 +8,12 @@ var (
 	ErrInvalidRequestBody = errors.New("invalid request body")
 )
 
+type ValidationErrors map[string]string
+
+func (v ValidationErrors) Error() string {
+	return "validation failed"
+}
+
 // Todo domain errors
 var (
 	ErrTodoNotFound = errors.New("todo not found")
